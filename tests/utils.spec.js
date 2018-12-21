94e741f6c448605ee5576fa8d2f949521969ac31
Luke Moderwell <luke.moderwell@gmail.com>
2018-12-21 12:21:14 -0500

    USXXXX: Jasmine Tests Utils -> class; run jasmine tests

--- tests/utils.spec.js
+++ tests/utils.spec.js
@@ -1,25 +1,22 @@
-// import * as Utils from "../utils"
+var Utilities = require('../utilities');
+var utils = new Utilities();
 
-describe("Cookie Utilities", function() {
-    
-    it("should set a cookie", function() {
-        // console.log(document.cookie);
-        Utils = require('../utils.js');
-        var url = "https://www.crossroads.net?coolparam=testit";
-        document = {
-            cookie: `redirect_url=${url};anotherstring=string`
-        };
-        Utils.getCookie("test");
-        // const result = Utils.getCookie("redirect_url");
+describe("Cookie Utilities", function () {
 
-        var url = "https://www.crossroads.net?coolparam=testit&anotherparam=fake";
-        expect(result).toEqual();
-      });
+  it("should set a cookie", function () {
+      // console.log(document.cookie);
+      var url = "https://www.crossroads.net?coolparam=testit";
+      document = {
+          cookie: `redirect_url=${url};anotherstring=string`
+      };
+      const result = utils.getCookie("redirect_url");
+      expect(result).toEqual(url);
+  });
 
-    it("should delete a cookie", function() {
-        
-        // var spy = spyOn(oktaSignInWidget, functionCall);
-        
-        // expect(spy).not.toHaveBeenCalled()
-    });
+  it("should delete a cookie", function () {
+
+      // var spy = spyOn(oktaSignInWidget, functionCall);
+
+      // expect(spy).not.toHaveBeenCalled()
+  });
 });
