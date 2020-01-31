7e7ba4426f12f1e60d40e2425c20b33985fdec75
Sarah Henney <shenney@callibrity.com>
2020-01-31 17:00:50 -0500

    Moved test kickoff scripts into test folder

--- integration_tests/teamcity_run_tests.sh
+++ integration_tests/teamcity_run_tests.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+# TEAMCITY_TEST_BUILD must equal the Teamcity build id, ex "WebApps_SignInPage_2Development"
 if [[ -z "${TEAMCITY_TEST_BUILD}" ]];
 then
-	echo "DEBUG Not running live integration tests"
   exit 0
 fi
 
-echo "DEBUG Starting live integration tests"
 curl --location --request POST 'https://ci.crossroads.net/app/rest/buildQueue' \
 --header 'Content-Type: application/xml' \
 --header 'Origin: https://ci.crossroads.net' \
